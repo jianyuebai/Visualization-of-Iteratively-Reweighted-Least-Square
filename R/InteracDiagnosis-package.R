3a4bc754b09512b013be9baebe14e81a3c4cdb5a
Jianyue Bai <jianyue.bai@mail.utoronto.ca>
2025-12-15 22:31:11 -0500

    doc

---
+++ R/InteracDiagnosis-package.R
@@ -0,0 +1,9 @@
+#' InteracDiagnosis
+#'
+#' A Shiny tool for comparing linear models using coefficient shifts, bootstrap
+#' standard errors, partial plots, and optional cross-validation.
+#'
+#' @docType package
+#' @name InteracDiagnosis
+"_PACKAGE"
+
